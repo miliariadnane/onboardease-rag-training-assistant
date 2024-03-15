334733a5fa5076faae437a8c3d7dc2dc17519f6e
Adnane Miliari <adnane.miliari@transperfect.com>
2024-03-15 23:02:46 +0000

    💿 Add ChatMemory to ConversationalRetrievalChain configuration

--- src/main/java/dev/nano/tptragbot/LangChainConfiguration.java
+++ src/main/java/dev/nano/tptragbot/LangChainConfiguration.java
@@ -2,6 +2,8 @@ package dev.nano.tptragbot;
 
 import dev.langchain4j.chain.ConversationalRetrievalChain;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
@@ -28,7 +30,11 @@ public class LangChainConfiguration {
     private Long timeout;
 
     @Bean
-    public ConversationalRetrievalChain chain(EmbeddingStore<TextSegment> embeddingStore, EmbeddingModel embeddingModel) {
+    public ConversationalRetrievalChain chain(
+            EmbeddingStore<TextSegment> embeddingStore,
+            EmbeddingModel embeddingModel,
+            ChatMemory chatMemory
+    ) {
         log.info("Creating ConversationalRetrievalChain bean");
         return ConversationalRetrievalChain.builder()
                 .chatLanguageModel(OpenAiChatModel.builder()
@@ -38,6 +44,7 @@ public class LangChainConfiguration {
                 )
                 //.promptTemplate(PromptTemplate.from(PROMPT_TEMPLATE))
                 .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
+                .chatMemory(chatMemory)
                 .build();
     }
 
@@ -51,5 +58,13 @@ public class LangChainConfiguration {
     public EmbeddingStore<TextSegment> embeddingStore() {
         log.info("Creating EmbeddingStore bean");
         return new InMemoryEmbeddingStore<>();
+    }
+
+    // chatMemory is a bean that is used to store the chat history
+    // It allows the chat to remember the context of the conversation which enhance conversation context understanding
+    @Bean
+    public MessageWindowChatMemory chatMemory() {
+        log.info("Creating MessageWindowChatMemory bean");
+        return MessageWindowChatMemory.withMaxMessages(20);
     }
 }
